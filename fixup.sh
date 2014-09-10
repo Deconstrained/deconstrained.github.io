bf728450691374df809469691647ccac2a03cd24
Demitri Morgan <demitri.morgan@gmail.com>
2014-09-10 13:57:25 -0700

    Changed index to full-width page

--- fixup.sh
+++ fixup.sh
@@ -3,4 +3,4 @@
 sed -i 's:Technologies got to work on:Tools and Languages:g' *.html
 sed -i 's%http://code.jquery.com/jquery-1.10.1.min.js%//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js%g' *.html
 cp index.html frame.html
-cp core-page.html index.html
+cp page.html index.html
