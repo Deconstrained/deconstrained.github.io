79c7b85737996a3d8cca7200063a9f509487b941
Demitri Morgan <demitri.morgan@gmail.com>
2017-12-27 19:52:17 -0800

    Removed a bunch of old unused files

--- old/fixup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i 's:Technologies got to work on:Tools and Languages:g' *.html
-sed -i 's%http://code.jquery.com/jquery-1.10.1.min.js%//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js%g' *.html
-cp index.html frame.html
-cp page.html index.html
